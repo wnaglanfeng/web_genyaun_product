a362ae489315f73da25d36165edf6291db894c45
lancy <wanglanfeng@igenyuan.com>
2026-06-06 21:20:20 +0800

    fix: add allowedHosts for fc.devsapp.net

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '.fcv3.1473200263418811.cn-hangzhou.fc.devsapp.net',
+    ],
+  },
 })
